2a174544882ee714d770dac3f5c551490582a29e
Nashs <aloise.depelley@insa-cvl.fr>
2017-04-12 11:29:03 +0200

    releases

---
+++ ad/src/main/java/ad/App.java
@@ -0,0 +1,23 @@
+package ad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World !" );
+        System.out.println("On appel la fonction max : ");
+        System.out.println("Max = "+max(4,5));
+    }
+
+
+    public static int max(int a, int b){
+    	return a;
+    }
+
+    
+    
+}
